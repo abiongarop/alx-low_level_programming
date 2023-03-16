7003b220504b78889f65742f683e859026c22f49
Your Name <you@example.com>
2023-03-16 10:21:53 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Prints a function followed by a new line
+ *
+ * Return: 0 (Success)
+ */
+int main (void)
+{
+printf ("\"with proper grammar, but the outcome is a piece of art,");
+return (0);
+}
