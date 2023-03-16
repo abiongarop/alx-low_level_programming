3ba0deffb740f62fd04c888b456572cd47e642ef
Your Name <you@example.com>
2023-03-16 11:27:09 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -9,14 +9,12 @@ int main(void)
 {
 	char c;
 	int i;
-	double d;
 	long int l;
 	long long int a;
 	float f;
 
 printf("The size of a char is: %lu.\n", (unsigned long)sizeof(c));
 printf("The size of an int is: %lu.\n", (unsigned long)sizeof(i));
-printf("The size of a double is: %lu.\n", (unsigned long)sizeof(d));
 printf("The size of a long int is: %lu.\n", (unsigned long)sizeof(l));
 printf("The size of a long long int is: %lu.\n", (unsigned long)sizeof(a));
 printf("The size of a float is: %lu.\n", (unsigned long)sizeof(f));
