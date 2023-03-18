c40a1690c48d54d895bb40cd06a3b9bf13cea1bf
Your Name <you@example.com>
2023-03-17 19:16:22 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main = a program that prints all single digit numbers of base 10 starting from 0, followed by a new line.
+ *
+ */
+int main(void)
+{
+	int d;
+	for (d = '0'; d < '9'; d++)
+	putchar(d);
+	putchar('\n');
+return (0);
+}
