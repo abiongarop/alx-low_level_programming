f4a696aca9a395a127f469636051d6b3350145d6
abiongarop <abiongmary610@gmail.com>
2023-03-22 18:59:42 -0700

    7-print_last_digit.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * int_abs - computes the absolute value of an integer.
+ * @c: integer to be computed
+ * Returns: absolute value or 0
+ */
+int _abs(int)
+{
+	if (c < 0)
+	{
+	int abs_val;
+
+	abs_val = c * -1;
+	return (abs_val);
+	}
+	return (c);
+}
