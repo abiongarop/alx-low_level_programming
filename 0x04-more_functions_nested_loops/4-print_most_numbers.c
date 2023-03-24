b50d7c7bf9fccd052e83e1006fd831ebee190277
abiongarop <abiongmary610@gmail.com>
2023-03-24 16:01:17 -0700

    10-print_triangle.c git commit -m 10-print_triangle.c

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - prints the numbers, from 0 to 9
+ * Return: numbers from 0 to 9 except 2 and 4
+ */
+
+void print_most_numbers(void)
+{
+	int n = 0;
+
+	for (; n <= 9; n++)
+	{
+	if (n == 2 || n == 4)
+	{
+		continue;
+	}
+	else
+	{
+	return (0);
+	}
+	}
+	_putchar('\n');
+}
+
