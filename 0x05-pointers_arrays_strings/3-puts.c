7f745575a135300cf2a4c3f38477e399130aa23f
abiongarop <abiongmary610@gmail.com>
2023-03-29 13:12:43 -0700

    3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _puts - prints a string, followed by a new line
+ * @str: string to be printed
+ * Return: void
+ */
+
+void _puts(char *str)
+{
+	for (; *str != '\0'; str++)
+	{
+		putchar(*str);
+	}
+	putchar('\n');
+}
