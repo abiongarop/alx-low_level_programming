c3fb49d9f9f3974c50af2c0da8d747ed09e7080a
abiongarop <abiongmary610@gmail.com>
2023-04-18 13:03:28 -0700

    1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - prints the number of arguments passed
+ * @argc: number of arguments
+ * @argv: array of strings
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	(void) argv; 
+	printf("%d\n", argc - 1);
+	return (0);
+}
